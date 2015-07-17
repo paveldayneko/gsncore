6672f8bf7d77a69ca34dd975af9c6a1e5abeabd0
noogen <tom@noogen.net>
2015-07-17 07:50:17 -0500

    add more info

--- gsncore-basic.js
+++ gsncore-basic.js
@@ -2,7 +2,7 @@
  * gsncore
  * version 1.6.3
  * gsncore repository
- * Build date: Fri Jul 17 2015 07:46:34 GMT-0500 (CDT)
+ * Build date: Fri Jul 17 2015 07:50:06 GMT-0500 (CDT)
  */
 ; (function () {
   'use strict';
@@ -551,7 +551,7 @@
             extra.ailse = item.AisleName;
         }
 
-        _tk.event(properties.category, action, properties.label, null, properties.value);
+        _tk.event(properties.category, action, properties.label, null, properties.value, properties.item);
       }
     });
   };
