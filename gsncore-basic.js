2c6f16dee16a99003f7d90beb38029f6e1cb2ae1
noogen <noogen@yahoo.com>
2015-10-19 02:05:14 -0500

    rebuild

--- gsncore-basic.js
+++ gsncore-basic.js
@@ -1,8 +1,8 @@
 /*!
  * gsncore
- * version 1.6.12
+ * version 1.6.13
  * gsncore repository
- * Build date: Tue Oct 13 2015 11:46:07 GMT-0500 (CDT)
+ * Build date: Mon Oct 19 2015 02:04:34 GMT-0500 (Central Daylight Time)
  */
 ; (function () {
   'use strict';
