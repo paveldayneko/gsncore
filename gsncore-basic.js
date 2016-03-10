bc95c1e72b83848bc1172460393db8b4e64bd9f9
noogen <tom@noogen.net>
2016-03-10 14:28:20 -0600

    updates

--- gsncore-basic.js
+++ gsncore-basic.js
@@ -1,8 +1,8 @@
 /*!
  * gsncore
- * version 1.7.11
+ * version 1.7.12
  * gsncore repository
- * Build date: Thu Mar 10 2016 14:27:39 GMT-0600 (CST)
+ * Build date: Thu Mar 10 2016 14:28:10 GMT-0600 (CST)
  */
 ;(function() {
   'use strict';
